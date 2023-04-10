efe5e3cf56858507b122a2f51ab37b086e7af423
tnishamon <92564751+tnishamon@users.noreply.github.com>
2023-04-10 12:54:44 -0700

    Code for discord bot (packages not included)

---
+++ discordBot/Program.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+public class Program
+{
+	private DiscordSocketClient _client;
+
+	public static void Main(string[] args)
+		=> new Program().MainAsync().GetAwaiter().GetResult();
+
+	public async Task MainAsync()
+	{
+		_client = new DiscordSocketClient();
+		_client.Log += Log;
+		await _client.LoginAsync(TokenType.Bot,
+			"OTM4Mjc5MzI5NDY3NjIxMzk2.Yfn-uA.lhfdaqoT144q1JIBwIQ2CVScZsw");
+		await _client.StartAsync();
+
+		// Block this task until the program is closed.
+		await Task.Delay(-1);
+	}
+	private Task Log(LogMessage msg)
+	{
+		Console.WriteLine(msg.ToString());
+		return Task.CompletedTask;
+	}
+}
+
